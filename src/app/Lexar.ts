66abb82e4746eef4a65954877210cfe22214ef89
Marc FLAVIUS <marc_flavius@yahoo.fr>
2023-05-20 03:23:08 +0200

    [main] 📦 NEW: add math operator capture token

--- src/app/Lexar.ts
+++ src/app/Lexar.ts
@@ -1,6 +1,11 @@
 import _ from 'lodash-contrib';
 import { SyntaxKind, SyntaxToken } from './SyntaxToken.js';
-
+enum MathOperator {
+  multiply = '*',
+  addition = '+',
+  subtract = '-',
+  divide = '/',
+}
 interface LexarInt {
   nextToken(): void;
 }
@@ -8,10 +13,22 @@ export class Lexar implements LexarInt {
   private readonly _text: string;
   private _position: number;
   public words: any[];
+  private _operatorTokenMap: {
+    '*': SyntaxKind;
+    '+': SyntaxKind;
+    '-': SyntaxKind;
+    '/': SyntaxKind;
+  };
 
   constructor(text) {
     this._text = text;
     this._position = 0;
+    this._operatorTokenMap = {
+      [MathOperator.multiply]: SyntaxKind.multiplyToken,
+      [MathOperator.addition]: SyntaxKind.additionToken,
+      [MathOperator.subtract]: SyntaxKind.subtractToken,
+      [MathOperator.divide]: SyntaxKind.divideToken,
+    };
   }
   private get current() {
     if (this._position >= this._text.length) {
@@ -20,29 +37,48 @@ export class Lexar implements LexarInt {
     return this._text[this._position];
   }
 
-  public isNumeric(value: any) {
+  private isNumeric(value: any) {
     return _.isNumeric(value);
   }
-  public isWhiteSpace(value: any) {
+  private isWhiteSpace(value: any) {
     return value == ' ';
   }
-
+  public isOperator(value: any) {
+    return Object.keys(this._operatorTokenMap).includes(value);
+  }
+  private makeIsOperator(operator: any) {
+    return (value: any) => value === operator;
+  }
+  private makeSyntaxKind(operator: any) {
+    return SyntaxKind[this._operatorTokenMap[operator]];
+  }
   public nextToken(): SyntaxToken {
-    // <number>
-    // <operator>
-    // <whitespace>
     if (this.isNumeric(this.current)) {
       return this.captureGroupe(this.isNumeric, SyntaxKind.numberToken);
     }
     if (this.isWhiteSpace(this.current)) {
       return this.captureGroupe(this.isWhiteSpace, SyntaxKind.whiteSpaceToken);
     }
+    if (this.isOperator(this.current)) {
+      return this.captureGroupe(
+        this.makeIsOperator(this.current),
+        this.makeSyntaxKind(this.current),
+      );
+    }
     return undefined;
   }
 
-  private captureGroupe(onCondition: Function, syntaxKind: SyntaxKind) {
+  private captureGroupe(
+    onCondition: Function,
+    syntaxKind: SyntaxKind,
+    options = {
+      maxTokenLength: -1,
+    },
+  ) {
     const start = this._position;
-    while (onCondition(this.current)) {
+    while (
+      onCondition(this.current || options.maxTokenLength === this._position)
+    ) {
       this._position++;
     }
     const end = this._position;
--- src/app/SyntaxToken.ts
+++ src/app/SyntaxToken.ts
@@ -2,6 +2,11 @@ export enum SyntaxKind {
   numberToken = 'numberToken',
   stringToken = 'stringToken',
   whiteSpaceToken = 'whiteSpaceToken',
+  multiplyToken = 'multiplyToken',
+  additionToken = 'additionToken',
+  subtractToken = 'subtractToken',
+  divideToken = 'divideToken',
+  equalToken = 'equalToken',
 }
 export class SyntaxToken {
   kind: SyntaxKind;
